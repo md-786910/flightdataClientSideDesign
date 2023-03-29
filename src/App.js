ce83b66a38bc535f10933b4865bd7fb752f530b1
md ashif reza <76996887+md-786910@users.noreply.github.com>
2023-03-29 16:46:52 +0530

    download qotation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Navbar from "./components/Header/Header";
 import {
   BrowserRouter as Router,
@@ -13,46 +14,57 @@ import Cart from './pages/Cart';
 import Footer from './components/Footer/Footer';
 import Chat from './pages/Chat';
 import Quotation from './pages/Quotation';
+import axios from 'axios';
+import { API } from './pages/api';
 function App() {
-
-
   const [len, setLen] = useState(0);
+  const [render, setRender] = useState(0);
+
+
+  const fetchCart = async () => {
+    try {
+      const resp = await axios(`${API}/getCart`)
+      if (resp.data.success) {
+        setLen(resp.data.data)
+      } else {
+        return;
+      }
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+  const getLen = (props) => {
+    setRender(props);
+  }
+
+
+  useEffect(() => {
+    fetchCart();
+  }, [render])
+
 
   const ScrollTop = () => {
     const location = useLocation();
-
     useEffect(() => {
       window.scrollTo({
         top: 0,
         left: 0,
       });
-
-
     }, [location.pathname]);
     return null;
   };
-
-
-  const getLen = (props) => {
-    console.log(props);
-    setLen(props);
-
-  }
-
-
+  // <ScrollTop />
   return (
     <>
       <Router>
-        <ScrollTop />
-        <Navbar len={len.length} />
+        <Navbar len={len.length || 0} />
         <Routes>
-          <Route path="/" element={<Home key={Math.random()} getLen={getLen} />} />
+          <Route path="/" element={<Home getLen={getLen} />} />
           <Route path="/carts" element={<Cart getLen={getLen} />} />
           <Route path='/chat' element={<Chat />} />
           <Route path='/getQuotation' element={<Quotation />} />
-
         </Routes>
-
+        <ToastContainer />
       </Router>
 
 
